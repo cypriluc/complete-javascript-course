076c43063d33059831f7526bdf8deaa5e44eb642
Lucia Cyprianova <l.cyprianova@gmail.com>
2022-10-07 16:15:33 +0200

    start app dev

--- 18-forkify/my-forkify/src/js/controller.js
+++ 18-forkify/my-forkify/src/js/controller.js
@@ -11,3 +11,33 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca57'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    // let recipe = data.data.recipe;
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      serving: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredient,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
